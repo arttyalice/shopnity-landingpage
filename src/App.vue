69ee33a53011abbf23581d10a58c7e4deb82a064
Setthasit Thungkawee <arttyalice@hotmail.co.th>
2019-02-20 10:02:40 +0700

    change font to promt

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,14 @@ export default {
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css?family=Kanit:200,300,400,600');
+  @import url('https://fonts.googleapis.com/css?family=Prompt:200,300,400,600');
   body, html {
     margin: 0px;
     padding: 0px;
 
   }
   #app {
-    font-family: 'Kanit', Helvetica, Arial, sans-serif;
+    font-family: 'Prompt', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
